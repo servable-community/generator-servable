c5ce601cc0e51ca172c8dd48dbe655ea92744422
Aboubacar Doucoure <yelounak@gmail.com>
2023-09-24 11:51:37 +0200

    Removed inquirer

--- generators/app/registerInquirers.js
+++ generators/app/registerInquirers.js
@@ -4,12 +4,12 @@
 
 import inquirerPromptAutocomplete from 'inquirer-autocomplete-prompt'
 import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt'
-import inquirerCheckboxPlus from 'inquirer-checkbox-plus-prompt'
+// import inquirerCheckboxPlus from 'inquirer-checkbox-plus-prompt'
 import inquirerParseJsonFile from 'inquirer-parse-json-file'
 
 export default (generator) => {
     generator.env.adapter.promptModule.registerPrompt('autocomplete', inquirerPromptAutocomplete)
     generator.env.adapter.promptModule.registerPrompt('file-tree-selection', inquirerFileTreeSelection)
-    generator.env.adapter.promptModule.registerPrompt('checkbox-plus', inquirerCheckboxPlus)
+    // generator.env.adapter.promptModule.registerPrompt('checkbox-plus', inquirerCheckboxPlus)
     generator.env.adapter.promptModule.registerPrompt('json-file', inquirerParseJsonFile)
 }
